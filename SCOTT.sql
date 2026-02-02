1f6879d24a7d6049ab2c326ef19d15c9abb2acd4
taehotaeho <jackt0506@naver.com>
2026-02-02 10:21:50 +0900

    첫번째 커밋

---
+++ SCOTT.sql
@@ -0,0 +1,10 @@
+-- Oracle(DBMS) - version(21C) - xe(database명)
+-- user(scott) - 테이블.
+-- Structured Query Language (SQL)
+SELECT STUDNO, NAME-- 컬럼명(전체)
+FROM STUDENT; -- 테이블.
+
+-- 1) PROFESSOR 테이블. 전체 컬럼 조회.
+SELECT *
+FROM PROFESSOR;
+
