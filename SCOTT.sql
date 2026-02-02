bd142037d8582bea1e588f978866c91c93136348
Adina <134618865+AdinaLTH@users.noreply.github.com>
2026-02-02 11:13:34 +0900

    Update SCOTT.sql

--- SCOTT.sql
+++ SCOTT.sql
@@ -1,13 +1,15 @@
--- Oracle(DBMS) - version(21C) - xe(database��)
--- user(scott) - ���̺�.
+-- Oracle(DBMS) - version(21C) - xe(database명)
+-- user(scott) - 테이블.
 -- Structured Query Language (SQL)
-SELECT STUDNO, NAME-- �÷���(��ü)
-FROM STUDENT; -- ���̺�.
+SELECT STUDNO, NAME-- 컬럼명(전체)
+FROM STUDENT; -- 테이블.
 
--- 1) PROFESSOR ���̺�. ��ü �÷� ��ȸ.
+-- 1) PROFESSOR 테이블. 전체 컬럼 조회.
 SELECT *
 FROM PROFESSOR;
 
--- 2) �л� -> �л���ȣ, �̸�, �г�
+-- 2) 학생 -> 학생번호, 이름, 학년
 SELECT STUDNO, NAME, GRADE
 FROM STUDENT;
+
+-- 숙제완료함.
